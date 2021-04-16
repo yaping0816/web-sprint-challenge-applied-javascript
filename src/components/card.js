ddeae9ceb83896564c9c6ce312fbcfc8fdd5896f
Yaping Liu <yaping.liu12@outlook.com>
2021-04-16 16:51:05 -0700

    finished card.js

--- src/components/card.js
+++ src/components/card.js
@@ -1,3 +1,5 @@
+import axios from "axios";
+
 const Card = (article) => {
   // TASK 5
   // ---------------------
@@ -17,7 +19,41 @@ const Card = (article) => {
   //   </div>
   // </div>
   //
+  const card = document.createElement('div');
+  const headLine = document.createElement('div');
+  const author = document.createElement('div');
+  const imgContainer = document.createElement('div');
+  const img = document.createElement('img');
+  const span = document.createElement('span');
+  
+  card.classList.add('card');
+  headLine.classList.add('headline');
+  headLine.textContent = article.headline;
+  author.classList.add('author');
+  imgContainer.classList.add('img-container');
+  img.setAttribute('src', article.authorPhoto)
+  span.textContent = `By ${article.authorName}`;
+
+  card.appendChild(headLine);
+  card.appendChild(author);
+  author.appendChild(imgContainer);
+  author.appendChild(span);
+  imgContainer.appendChild(img);
+
+  card.addEventListener('click', event =>{
+    console.log(headLine.textContent);
+  })
+
+  return card;
 }
+
+// console.log(Card({
+//   "authorName": "FIDO WALKSALOT",
+//   "authorPhoto": "https://tk-assets.lambdaschool.com/1891c758-b3f4-4ec7-9d88-590bf7c7ceb2_fido.jpg",
+//   "headline": "Bootstrap 5: Get a Sneak Peak at all the New Features",
+//   "id": "3b04aebe-2097-4c70-9bb4-8a525c410bef"
+// }))
+
 
 const cardAppender = (selector) => {
   // TASK 6
@@ -28,6 +64,32 @@ const cardAppender = (selector) => {
   // Create a card from each and every article object in the response, using the Card component.
   // Append each card to the element in the DOM that matches the selector passed to the function.
   //
+  axios.get('https://lambda-times-api.herokuapp.com/articles')
+  .then(res =>{
+    // const bootstrapArray = res.data.articles.bootstrap;
+    // bootstrapArray.forEach(object => {
+    //   const newCard = Card(object);
+    //   const cardsContainer = document.querySelector(selector);
+    //   cardsContainer.appendChild(newCard);
+    // });
+
+    // cardsMaker(res.data.articles.bootstrap, selector);
+    Object.keys(res.data.articles).forEach(key =>{
+      cardsMaker(res.data.articles[key], selector);
+    })
+  })
+  .catch(err =>{
+    console.log(err);
+  })
+}
+
+const cardsMaker = (dataArray, selector) =>{
+
+  dataArray.forEach(object =>{
+    const newCard = Card(object);
+    const cardsContainer = document.querySelector(selector);
+    cardsContainer.appendChild(newCard);
+  })
 }
 
 export { Card, cardAppender }
